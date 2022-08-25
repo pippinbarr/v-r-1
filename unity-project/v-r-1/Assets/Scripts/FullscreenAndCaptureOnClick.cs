9b843995fb61eb64c2871b615bdec05a8a4b6374
Pippin Barr <pippin.barr@gmail.com>
2022-08-25 10:42:06 -0400

    Created webgl, mac, windows builds

---
+++ unity-project/v-r-1/Assets/Scripts/FullscreenAndCaptureOnClick.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class FullscreenAndCaptureOnClick : MonoBehaviour
+{
+
+    private bool ready = false;
+
+    // Use this for initialization
+    void Start()
+    {
+	
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !ready)
+        {
+            // Get highest resolution supported by the current screen.
+            Resolution resolution = Screen.resolutions [Screen.resolutions.Length - 1];
+ 
+            // The last parameter "true" denotes if it should be fullscreen or not.
+
+            Screen.SetResolution(resolution.width, resolution.height, true);
+
+            Screen.fullScreen = true;
+
+            Screen.lockCursor = true;
+
+            ready = true;
+
+            StartCoroutine(NextScene());
+
+        }
+    }
+
+    IEnumerator NextScene()
+    {
+
+        yield return new WaitForSeconds(0.5f);
+        SceneManager.LoadScene("_Title");
+    }
+}
---
+++ unity-project/v-r-1/Assets/Scripts/GlobalControl.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlobalControl : MonoBehaviour
+{
+
+    //    public string[] theLevels = new string[2]
+    //    {
+    //        "Intersection",
+    //        "Door down",
+    ////        "Door in floor",
+    ////        "Door to door",
+    //    };
+
+    //    public int[] theLevelNumbers = new int[2]
+    //    {
+    //        2,
+    //        3,
+    ////        3,
+    ////        4,
+    //    };
+
+    
+    public string[] theLevels = new string[15]
+    {
+        "Intersection",
+        "Door down",
+        "Door in floor",
+        "Door to door",
+        "Floating objects",
+        "Floating room",
+        "Hole in floor",
+        "Lean",
+        "Locked out",
+        "No light",
+        "Organised",
+        "Pieces",
+        "Sunken",
+        "Upside down",
+        "Window to window",
+    };
+    
+    public int[] theLevelNumbers = new int[15]
+    {
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9,
+        10,
+        11,
+        12,
+        13,
+        14,
+        15
+    };
+
+    public int currentLevel = -1;
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log(theLevels.Length);
+        DontDestroyOnLoad(gameObject);  
+        reshuffle(theLevels, theLevelNumbers);
+        for (int i = 0; i < theLevels.Length; i++)
+        {
+            Debug.Log(theLevels [i]);
+        }
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {
+	
+    }
+
+    void reshuffle(string[] texts, int[] levelNumbers)
+    {
+        // Knuth shuffle algorithm :: courtesy of Wikipedia :)
+        for (int t = 0; t < texts.Length; t++)
+        {
+            string tmp = texts [t];
+            int r = Random.Range(t, texts.Length);
+            texts [t] = texts [r];
+            texts [r] = tmp;
+
+            int tmp2 = levelNumbers [t];
+            levelNumbers [t] = levelNumbers [r];
+            levelNumbers [r] = tmp2;
+        }
+    }
+}
